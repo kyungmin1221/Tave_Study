73fa0f8d7298583800d4330e1c976e6b5a264a9c
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-09-21 16:31:20 +0900

    2주차 (spring 배포이해)

---
+++ 2주차/study-spring-boot/src/main/java/com/in28minutes/studyspringboot/CourseController.java
@@ -0,0 +1,25 @@
+package com.in28minutes.studyspringboot;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn DevOps", "in28minutes"),
+                new Course(3, "Learn Azure", "in28minutes"),
+                new Course(4, "Learn GCP", "in28minutes"),
+                new Course(5,"java spring","in28minutes")
+
+        );
+    }
+
+}
---
+++ 2주차/study-spring-boot/src/test/java/com/in28minutes/studyspringboot/StudySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.studyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
