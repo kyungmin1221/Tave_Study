2b56e85301eb8f6ef13ab73ab80221a3d6fc89f9
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-09-23 16:57:04 +0900

    게시판(#3 ing)

--- 게시판/board/src/main/java/shop/pingping2/board/domain/Board.java
+++ 게시판/board/src/main/java/shop/pingping2/board/domain/Board.java
@@ -14,9 +14,13 @@ import javax.persistence.*;
 @Entity
 @Table(name = "board")  // 이거 보고 테이블 생성
 public class Board extends Time {
+    // Board : 실제 DB와 연결될 클래스 (Entity Class)
+    // JPA에서는 프록시 생성을 위해 기본 생성자를 반드시 하나 생성해야 한다/
+    // 생성자 자동 생성 : NoArgsConstructor, AllArgsConstructor
+    // NoArgsConstructor : 객체 생성 시 초기 인자 없이 객체를 생성할 수 있다.
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id     // 해당 Property가 Primary 키의 역할을 한다는 것을 나타냄
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // Primary Key의 값을 위한 자동 생성 전략을 명시하는데 사용한다.
     private Long id;
     @Column(length = 10,nullable = false)
     private String writer;
@@ -24,6 +28,9 @@ public class Board extends Time {
     private String title;
     @Column(columnDefinition = "Text", nullable = false)
     private String content;
+
+    // @OneToOne :  Board가 필드값으로 갖고 있는 User 도메인을 1:1 관계로 설정하는 Annotation 이다.
+    //lazy : User 객체를 조회하는 시점이 아닌 객체가 실제 사용될 때 조회한다는 뜻이다. lazy와 eager 가 있다.
     @OneToOne(fetch = FetchType.LAZY)
     private User user;
 
--- 게시판/board/src/main/java/shop/pingping2/board/domain/User.java
+++ 게시판/board/src/main/java/shop/pingping2/board/domain/User.java
@@ -2,7 +2,24 @@ package shop.pingping2.board.domain;
 
 // Google OAuth2 로그인 한 사용자들에게 대한 정보를 저장하기 위한 User 테이블
 
-public class User {
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+//@Entity(name = "USER_TABLE")
+@Entity
+@Table(name ="user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)      // 외부에서의 생성을 열어 둘 필요가 없을 때 / 보안적으로 권장된다.
+public class User extends Time {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+
 
 }
 
