a48c956187a29b73038b6a696ed4ba1856d32fbc
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-10-07 12:29:59 +0900

    todoApp

---
+++ todoApp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/MyfirstwebappApplication.java
@@ -0,0 +1,15 @@
+package com.in28minutes.springboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// application 을 실행하기 위한 클래스(파일)  //
+
+@SpringBootApplication
+public class MyfirstwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfirstwebappApplication.class, args);
+    }
+
+}
