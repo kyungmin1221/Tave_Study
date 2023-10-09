e50f1155e8a45e909a7ab28396f2c606ce07181a
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-10-09 21:31:29 +0900

    jsp

--- todoApp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ todoApp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,14 +1,21 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     // login => com.in28minutes.springboot.myfirstwebapp.login.LoginContoller => login.jsp
+
+
+    // http://localhost:8080/login?name=kyungmin
     @RequestMapping("login")
-    public String gotoLoginPage() {
+    public String gotoLoginPage(@RequestParam String name, ModelMap model) {
+        model.put("name",name);
+        System.out.println("Requset param is " + name);
         return "login";
     }
 }
