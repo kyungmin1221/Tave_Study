af5ae99567a2c5080b6a80b49d5c23a27b12e535
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-10-11 03:24:35 +0900

    update 일부 구현

--- todoApp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ todoApp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -35,5 +36,19 @@ public class TodoService {
         Predicate<? super Todo> predicate
                 = todo -> todo.getId() == id;
         todos.removeIf(predicate);
+    }
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate
+                = todo -> todo.getId() == id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+
+        return todo;
+    }
+
+    public void updateTodo(@Valid Todo todo) {
+        deleteById(todo.getId());
+        todos.add(todo);
+
     }
 }
--- todoApp/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
+++ todoApp/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -16,6 +16,7 @@
                     <th>Target Date</th>
                     <th>Is Done?</th>
                     <th></th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -26,6 +27,7 @@
                         <td>${todo.targetDate}</td>
                         <td>${todo.done}</td>
                         <td><a href="delete-todo?id=${todo.id}" class="btn-warning">DELETE</a></td>
+                        <td><a href="update-todo?id=${todo.id}" class="btn-success">UPDATE</a></td>
                     </tr>
                 </c:forEach>
             </tbody>
