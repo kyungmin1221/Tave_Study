fd45cf88ac3f51208580775deeaa63229ffcb60e
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-10-11 19:00:46 +0900

    사용자 추가하기

--- todoApp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ todoApp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -15,21 +15,24 @@ public class SpringSecurityConfiguration {
 
     // 사용자 이름과 pwd를 저장하려고 할 때는 LDAP 또는 데이터베이스를 사용
 
-//    InMemoryUserDetailsManager
-//    public InMemoryUserDetailsManager() { /* compiled code */ }
-
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
+        UserDetails userDetails1 = createNewUser("kyungmin", "sun");
+        UserDetails userDetails2 = createNewUser("kyusun", "bb");
+        return new InMemoryUserDetailsManager(userDetails1,userDetails2);
+    }
+
+    private UserDetails createNewUser(String username,String password) {
         Function<String,String> passwordEncorder
                 =input -> passwordEncoder().encode(input);
+
         UserDetails userDetails = User.builder()
                                     .passwordEncoder(passwordEncorder)
-                                    .username("kyungmin")
-                                    .password("sun")
+                                    .username(username)
+                                    .password(password)
                                     .roles("USER","ADMIN")
                                     .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
