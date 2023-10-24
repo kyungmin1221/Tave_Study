e10f7b6db8409ea12ea0803e240d695675bfecee
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-10-24 11:28:18 +0900

    AOP

---
+++ 5주차/learn-spring-app/src/main/java/com/in28minutes/learnspringapp/LearnSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learnspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringAppApplication.class, args);
+    }
+
+}
