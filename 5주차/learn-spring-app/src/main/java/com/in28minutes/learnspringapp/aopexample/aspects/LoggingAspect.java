9ee5d923e797da666f1edbeeb02be7b680f51b39
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2023-10-24 14:27:25 +0900

    AOP-aspect

--- 5주차/learn-spring-app/src/main/java/com/in28minutes/learnspringapp/aopexample/aspects/LoggingAspect.java
+++ 5주차/learn-spring-app/src/main/java/com/in28minutes/learnspringapp/aopexample/aspects/LoggingAspect.java
@@ -1,6 +1,8 @@
 package com.in28minutes.learnspringapp.aopexample.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +12,10 @@ import org.springframework.context.annotation.Configuration;
 public class LoggingAspect {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    public void logMethodCall() {
-        
+    // 명시한 패키지에 존재하는 빈으로 들어가는 메소드 호출을 인터셉트하라는 실행명령 //
+    // execution(* com.in28minutes.learnspringapp.aopexample.business.*.*(..))
+    @Before("execution(* com.in28minutes.learnspringapp.aopexample.data.*.*(..))")
+    public void logMethodCall(JoinPoint joinPoint) {
+        logger.info("Before Aspect - Method is called -> {}",joinPoint);
     }
 }
